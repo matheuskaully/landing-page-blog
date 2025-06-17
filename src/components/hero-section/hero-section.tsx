94e1e8d63b59dab62fb85c6366f8f1bb7a54874c
Matheus Kaúlly <kaully.santos@gmail.com>
2025-06-17 16:29:27 -0300

    feat: adiciona hero section

---
+++ src/components/hero-section/hero-section.tsx
@@ -0,0 +1,60 @@
+import { ArrowRight, Clock, Store } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Button } from '../ui/button'
+import heroImg from '../../../public/hero-section.svg'
+
+export function HeroSection() {
+  return (
+    <section className="container relative mt-16 flex items-center justify-center">
+      <div className="grid min-h-[20rem] grid-cols-1 items-center gap-8 md:h-[36rem] md:grid-cols-2">
+        <div className="flex flex-col items-center justify-center gap-4 md:items-start md:gap-8 lg:items-start">
+          <h1 className="font-alt text-heading-hg text-gray-100">
+            Venda seus produtos como afiliado em um único lugar
+          </h1>
+
+          <div className="flex flex-col items-center justify-center gap-4 md:items-start lg:items-start">
+            <div>
+              <div className="flex items-center gap-2">
+                <Clock className="h-4 w-4 text-cyan-100" />
+                <span className="text-gray-200">
+                  Crie o seu site em menos de 5 minutos
+                </span>
+              </div>
+
+              <div className="flex items-center gap-2">
+                <Store className="h-4 w-4 text-cyan-100" />
+                <span className="text-gray-200">
+                  Acompanhe e otimize seu negócio online
+                </span>
+              </div>
+            </div>
+
+            <div className="mt-5 flex flex-col items-center gap-2 text-white md:items-start lg:items-start">
+              <Button className="w-fit rounded-full" asChild>
+                <Link href="/criar-loja">
+                  Criar loja grátis
+                  <ArrowRight />
+                </Link>
+              </Button>
+
+              <p className="text-body-xs text-gray-300">
+                Não precisa de cartão de crédito
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="relative order-first hidden h-[20rem] items-center justify-center md:order-last md:flex md:h-full lg:flex">
+          <Image
+            src={heroImg}
+            alt="Ilustração com ícones de store, tag e sacola"
+            width={200}
+            height={400}
+            className="h-full w-auto object-contain"
+          />
+        </div>
+      </div>
+    </section>
+  )
+}
