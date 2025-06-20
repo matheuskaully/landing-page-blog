feb3355cdbbccd1e18bbb4081c77617dc6e0d5ff
Matheus Kaúlly <kaully.santos@gmail.com>
2025-06-20 17:37:15 -0300

    refactor: ajuste no input search

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -8,16 +8,13 @@ export function Search() {
   return (
     <div
       className={cn(
-        'group flex h-fit w-full items-center gap-3 rounded-lg border px-4 py-2 focus-within:border-blue-200 md:w-fit',
-        searchQuery.length > 0 ? 'border-blue-200' : 'border-gray-400',
+        'group flex h-fit w-full items-center gap-3 rounded-lg px-4 py-2 ring-1 focus-within:ring-blue-200 md:w-fit',
+        searchQuery.length > 0 ? 'ring-blue-200' : 'ring-gray-400',
       )}
     >
       <div>
         <SearchIcon
-          className={
-            (cn('size-4 group-focus-within:border-blue-200'),
-            searchQuery.length > 0 ? 'text-blue-200' : 'text-gray-300')
-          }
+          className={'size-5 text-gray-300 group-focus-within:text-blue-200'}
         />
       </div>
       <input
