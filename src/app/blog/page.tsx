d1af674656a33007adcf102b526fd8067cb60a35
Matheus Kaúlly <kaully.santos@gmail.com>
2025-06-30 14:11:08 -0300

    feat: adiciona fake delay

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,9 +1,16 @@
 import { BlogList } from '@/templates/blog'
 import { allPosts } from 'contentlayer/generated'
 
-export default function BlogListPage() {
+function fakeDelay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+export default async function BlogListPage() {
+  await fakeDelay(500)
+
   const sortedPosts = allPosts.sort(
     (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime(),
   )
+
   return <BlogList posts={sortedPosts} />
 }
