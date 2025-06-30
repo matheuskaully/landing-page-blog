087e59b3a8dd284f8b68d272b15f45849ea5d78b
Matheus Kaúlly <kaully.santos@gmail.com>
2025-06-30 10:51:00 -0300

    feat: ajustes para app router

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,9 @@
 import { BlogList } from '@/templates/blog'
+import { allPosts } from 'contentlayer/generated'
 
-export default function BlogPage() {
-  return <BlogList posts={[]} />
+export default function BlogListPage() {
+  const sortedPosts = allPosts.sort(
+    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime(),
+  )
+  return <BlogList posts={sortedPosts} />
 }
