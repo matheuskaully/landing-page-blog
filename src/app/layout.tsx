bb39d53e03ca0ca8d6dc697da1265c122a4ff0e7
Matheus Kaúlly <kaully.santos@gmail.com>
2025-07-01 11:42:27 -0300

    chore: adiciona metadados nos posts do blog

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,22 @@ import '@/styles/globals.css'
 export const metadata = {
   title: 'Site.Set',
   description: 'Venda seus produtos como afiliado em um único lugar!',
+  openGraph: {
+    title: 'Site.Set',
+    url: 'https://site-set-today.vercel.app/og-image.jpg',
+    siteName: 'Site.Set',
+    description: 'Venda seus produtos como afiliado em um único lugar!',
+    locale: 'pt_BR',
+    type: 'website',
+    images: [
+      {
+        url: 'https://site-set-today.vercel.app/og-image.jpg',
+        width: 800,
+        height: 600,
+        alt: 'Site.Set',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
